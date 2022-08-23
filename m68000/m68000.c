bdbd8d1c6bd72eced765281562689d19513b541b
negativeExponent <negativeExponent@users.noreply.github.com>
2022-08-23 19:41:07 +0800

    Cleanup remnants of last merge conflict

--- m68000/m68000.c
+++ m68000/m68000.c
@@ -42,18 +42,8 @@ unsigned int read16(unsigned int a) {
 
 unsigned int MyCheckPc(unsigned int pc)
 {
-
-
-<<<<<<< HEAD
-  pc-= m68k.membase; // Get the real program counter
-/*
-  if (!tracing) {
-  	//p6logd("CheckPC 0x%08X\n", pc);
-  }
-*/
-=======
   pc-= m68k.membase; /* Get the real program counter */
->>>>>>> 8b3341b (Add C68K cpu (originally used by px68x))
+
   if (pc <= 0xbfffff) 			       					{ m68k.membase=(int) MEM; return m68k.membase+pc; }
   if ((pc >= 0xfc0000) && (pc <= 0xffffff))	{ m68k.membase=(int) IPL - 0xfc0000; return m68k.membase+pc; }
   if ((pc >= 0xc00000) && (pc <= 0xc7ffff)) m68k.membase=(int) GVRAM - 0xc00000;
