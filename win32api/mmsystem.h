91df946b361c69d1644cfdaf109b34e644512ec1
negativeExponent <negativeExponent@users.noreply.github.com>
2022-08-30 13:37:44 +0800

    Replace typedefs in mmsystem.h with stdint types

--- win32api/mmsystem.h
+++ win32api/mmsystem.h
@@ -1,49 +1,45 @@
 /*	$Id: mmsystem.h,v 1.1.1.1 2003/04/28 18:06:55 nonaka Exp $	*/
 
-#ifndef	MMSYSTEM_H__
-#define	MMSYSTEM_H__
+#ifndef MMSYSTEM_H__
+#define MMSYSTEM_H__
 
 #include "windows.h"
 
-typedef	uint32_t	MMRESULT;
-typedef	HANDLE		HMIDIOUT;
-typedef	HMIDIOUT 	*LPHMIDIOUT;
+typedef struct midihdr
+{
+	uint8_t *lpData;
+	uint32_t dwBufferLength;
+	uint32_t dwBytesRecorded;
+	uint32_t dwUser;
+	uint32_t dwFlags;
+	struct midihdr *lpNext;
+	uint32_t reserved;
+	uint32_t dwOffset;
+	uint32_t dwReserved[8];
+} MIDIHDR;
 
-typedef struct midihdr {
-	uint8_t 		*lpData;
-	uint32_t		dwBufferLength;
-	uint32_t		dwBytesRecorded;
-	uint32_t		dwUser;
-	uint32_t		dwFlags;
-	struct midihdr	*lpNext;
-	uint32_t		reserved;
-	uint32_t		dwOffset;
-	uint32_t		dwReserved[8];
-} MIDIHDR, *PMIDIHDR, *NPMIDIHDR, *LPMIDIHDR;
+#define MMSYSERR_NOERROR     0
+#define MIDIERR_STILLPLAYING 2
 
+#define MIDI_MAPPER -1
 
-#define	MMSYSERR_NOERROR	0
-#define	MIDIERR_STILLPLAYING	2
-
-#define	MIDI_MAPPER		-1
-
-#define	CALLBACK_NULL		0x00000000L
-
+#define CALLBACK_NULL 0x00000000L
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-MMRESULT midiOutPrepareHeader(HMIDIOUT hmo, LPMIDIHDR pmh, uint32_t cbmh);
-MMRESULT midiOutUnprepareHeader(HMIDIOUT hmo, LPMIDIHDR pmh, uint32_t cbmh);
-MMRESULT midiOutShortMsg(HMIDIOUT hmo, uint32_t dwMsg);
-MMRESULT midiOutLongMsg(HMIDIOUT hmo, LPMIDIHDR pmh, uint32_t cbmh);
-MMRESULT midiOutOpen(LPHMIDIOUT phmo, uint32_t uDeviceID, uint32_t dwCallback, uint32_t dwInstance, uint32_t fdwOpen);
-MMRESULT midiOutClose(HMIDIOUT hmo);
-MMRESULT midiOutReset(HMIDIOUT hmo);
+uint32_t midiOutPrepareHeader(void *hmo, MIDIHDR *pmh, uint32_t cbmh);
+uint32_t midiOutUnprepareHeader(void *hmo, MIDIHDR *pmh, uint32_t cbmh);
+uint32_t midiOutShortMsg(void *hmo, uint32_t dwMsg);
+uint32_t midiOutLongMsg(void *hmo, MIDIHDR *pmh, uint32_t cbmh);
+uint32_t midiOutOpen(void **phmo, uint32_t uDeviceID, uint32_t dwCallback, uint32_t dwInstance, uint32_t fdwOpen);
+uint32_t midiOutClose(void *hmo);
+uint32_t midiOutReset(void *hmo);
 
 #ifdef __cplusplus
 };
 #endif
 
-#endif	/* MMSYSTEM_H__ */
+#endif /* MMSYSTEM_H__ */
