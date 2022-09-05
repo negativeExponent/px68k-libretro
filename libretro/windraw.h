4255ec9fbe4604135d50dbea6946e04d620ca5b2
negativeExponent <negativeExponent@users.noreply.github.com>
2022-09-05 16:22:33 +0800

    Cleanup windraw.c

--- libretro/windraw.h
+++ libretro/windraw.h
@@ -1,13 +1,11 @@
-#ifndef _winx68k_windraw_h
-#define _winx68k_windraw_h
+#ifndef _WINX68K_WINDRAW_H
+#define _WINX68K_WINDRAW_H
 
 extern uint16_t WinDraw_Pal16B, WinDraw_Pal16R, WinDraw_Pal16G;
 
 int WinDraw_Init(void);
 void WinDraw_Cleanup(void);
-void WinDraw_Redraw(void);
 void FASTCALL WinDraw_Draw(void);
-void WinDraw_ShowMenu(int flag);
 void WinDraw_DrawLine(void);
 void WinDraw_ChangeSize(void);
 
@@ -18,8 +16,5 @@ extern struct menu_flist mfl;
 
 void WinDraw_DrawMenufile(struct menu_flist *mfl);
 void WinDraw_ClearMenuBuffer(void);
-void WinDraw_reverse_key(int x, int y);
 
-#endif //winx68k_windraw_h
-
-
+#endif /* _WINX68K_WINDRAW_H */
