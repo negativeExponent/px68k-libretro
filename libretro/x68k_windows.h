4938e507ff0e06718ca3d4c4320b685c5d17c2d0
U-DESKTOP-RKN530O\negative <negativeExponent@users.noreply.github.com>
2024-07-18 12:09:18 -0700

    add missing files

---
+++ libretro/x68k_windows.h
@@ -0,0 +1,63 @@
+#ifndef	__NP2_WIN32EMUL_H__
+#define	__NP2_WIN32EMUL_H__
+
+#include <sys/param.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <string.h>
+#include <limits.h>
+#include <errno.h>
+#include <assert.h>
+
+#ifdef HAVE_C68k
+#include "/m68000/c68k/core.h"
+#endif
+
+#ifndef FASTCALL
+#define FASTCALL
+#endif
+
+#ifndef	MAX_PATH
+#define	MAX_PATH	MAXPATHLEN
+#endif
+
+/*
+ * DUMMY DEFINITION
+ */
+
+#ifndef	INLINE
+#ifdef _WIN32
+#define	INLINE	_inline
+#else
+#define	INLINE	inline
+#endif
+#endif
+
+/* for dosio.c */
+#define	X68K_GENERIC_READ			1
+#define	X68K_GENERIC_WRITE			2
+
+#define	X68K_OPEN_EXISTING			1
+#define	X68K_CREATE_ALWAYS			2
+
+/*
+ * replace
+ */
+#define	timeGetTime()		FAKE_GetTickCount()
+
+/*
+ * prototype
+ */
+#ifdef __cplusplus
+extern "C" {
+#endif
+int x68k_WritePrivateProfileString(const char *, const char *, const char *, const char*);
+#ifdef __cplusplus
+};
+#endif
+
+#include "peace.h"
+
+#endif /* __NP2_WIN32EMUL_H__ */
