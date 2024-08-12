931af42df0a27e0c7937b29ef0d2bb8553358a23
negativeExponent <negativeExponent@users.noreply.github.com>
2024-08-12 15:04:53 +0800

    merge pal_read/pal_write into vctrl read/write

--- x68k/palette.h
+++ x68k/palette.h
@@ -7,9 +7,6 @@ extern uint16_t GrphPal[256];
 extern uint16_t Pal16[65536];
 
 void Pal_Init(void);
-
-uint8_t FASTCALL Pal_Read(uint32_t adr);
-void FASTCALL Pal_Write(uint32_t adr, uint8_t data);
 void Pal_ChangeContrast(int num);
 
 extern uint16_t Ibit, Pal_HalfMask, Pal_Ix2;
