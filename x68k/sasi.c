fa227d39c571baf3c237216937ab0a9c3a142a81
negativeExponent <negativeExponent@users.noreply.github.com>
2024-08-13 11:06:28 +0800

    Update sasi.c

--- x68k/sasi.c
+++ x68k/sasi.c
@@ -115,85 +115,6 @@ static int SASI_Flush(void)
 	return 1;
 }
 
-uint8_t FASTCALL SASI_Read(uint32_t adr)
-{
-	uint8_t ret = 0;
-	int16_t result;
-
-	if (adr == 0xe96003)
-	{
-		if (SASI_Phase)
-			ret |= 2;                       /* Busy */
-		if (SASI_Phase > 1)
-			ret |= 1;                       /* Req */
-		if (SASI_Phase == 2)
-			ret |= 8;                       /* C/D */
-		if ((SASI_Phase == 3) && (SASI_RW)) /* SASI_RW=1:Read */
-			ret |= 4;                       /* I/O */
-		if (SASI_Phase == 9)                /* Phase=9:SenseStatus�� */
-			ret |= 4;                       /* I/O */
-		if ((SASI_Phase == 4) || (SASI_Phase == 5))
-			ret |= 0x0c;                    /* I/O & C/D */
-		if (SASI_Phase == 5)
-			ret |= 0x10;                    /* MSG */
-	}
-	else if (adr == 0xe96001)
-	{
-		if ((SASI_Phase == 3) && (SASI_RW)) /* Reading data ~ */
-		{
-			ret = SASI_Buf[SASI_BufPtr++];
-			if (SASI_BufPtr == 256)
-			{
-				SASI_Blocks--;
-				if (SASI_Blocks)             /* More blocks to read? */
-				{
-					SASI_Sector++;
-					SASI_BufPtr = 0;
-					result      = SASI_Seek(); /* Read next sector into buffer */
-					if (!result)               /* result=0: if it is the end of the image (= invalid sector) */
-					{
-						SASI_Error = 0x0f;
-						SASI_Phase++;
-					}
-				}
-				else
-					SASI_Phase++; /* Specified block read complete */
-			}
-		}
-		else if (SASI_Phase == 4) /* Status Phase */
-		{
-			if (SASI_Error)
-				ret = 0x02;
-			else
-				ret = SASI_Stat;
-			SASI_Phase++;
-		}
-		else if (SASI_Phase == 5) /* MessagePhase */
-		{
-			SASI_Phase = 0; /* Just return 0. Return to BusFree */
-		}
-		else if (SASI_Phase == 9) /* DataPhase(SenseStat����) */
-		{
-			ret = SASI_SenseStatBuf[SASI_SenseStatPtr++];
-			if (SASI_SenseStatPtr == 4)
-			{
-				SASI_Error = 0;
-				SASI_Phase = 4; /* StatusPhase�� */
-			}
-		}
-		if (SASI_Phase == 4)
-		{
-			IOC_IntStat |= 0x10;
-			if (IOC_IntStat & 8)
-				IRQH_Int(1, &SASI_Int);
-		}
-	}
-
-	StatBar_HDD((SASI_Phase) ? 2 : 0);
-
-	return ret;
-}
-
 /* Check the command. To be honest, the description in InsideX68k is not enough ^^;.
 * As for what is not described,
 * - C2h (initialization?). No parameters other than Unit. Write 10 pieces of data in DataPhase.
@@ -307,13 +228,70 @@ static void SASI_CheckCmd(void)
 	}
 }
 
-/* I/O Write */
-void FASTCALL SASI_Write(uint32_t adr, uint8_t data)
+static uint8_t SASI_ReadData(void)
 {
-	int result, i;
+	uint8_t ret = 0;
+	int result = 0;
+
+	if ((SASI_Phase == 3) && (SASI_RW)) /* Reading data ~ */
+	{
+		ret = SASI_Buf[SASI_BufPtr++];
+		if (SASI_BufPtr == 256)
+		{
+			SASI_Blocks--;
+			if (SASI_Blocks) /* More blocks to read? */
+			{
+				SASI_Sector++;
+				SASI_BufPtr = 0;
+				result = SASI_Seek(); /* Read next sector into buffer */
+				if (!result)          /* result=0: if it is the end of the image (= invalid sector) */
+				{
+					SASI_Error = 0x0f;
+					SASI_Phase++;
+				}
+			}
+			else
+				SASI_Phase++; /* Specified block read complete */
+		}
+	}
+	else if (SASI_Phase == 4) /* Status Phase */
+	{
+		if (SASI_Error)
+			ret = 0x02;
+		else
+			ret = SASI_Stat;
+		SASI_Phase++;
+	}
+	else if (SASI_Phase == 5) /* MessagePhase */
+	{
+		SASI_Phase = 0; /* Just return 0. Return to BusFree */
+	}
+	else if (SASI_Phase == 9) /* DataPhase(SenseStatExclusive) */
+	{
+		ret = SASI_SenseStatBuf[SASI_SenseStatPtr++];
+		if (SASI_SenseStatPtr == 4)
+		{
+			SASI_Error = 0;
+			SASI_Phase = 4; /* StatusPhaseへ */
+		}
+	}
+
+	if (SASI_Phase == 4)
+	{
+		IOC_IntStat |= 0x10;
+		if (IOC_IntStat & 8)
+			IRQH_Int(1, &SASI_Int);
+	}
+
+	return ret;
+}
+
+static void SASI_WriteData(uint32_t data)
+{
+	int i, result;
 	uint8_t bit;
 
-	if ((adr == 0xe96007) && (SASI_Phase == 0))
+	if (SASI_Phase == 0)
 	{
 		SASI_Device = 0x7f;
 		if (data)
@@ -337,74 +315,143 @@ void FASTCALL SASI_Write(uint32_t adr, uint8_t data)
 			SASI_Phase = 0;
 		}
 	}
-	else if ((adr == 0xe96003) && (SASI_Phase == 1))
+	else if (SASI_Phase == 2)
 	{
-		SASI_Phase++;
-	}
-	else if (adr == 0xe96005) /* SASI Reset */
-	{
-		SASI_Phase        = 0;
-		SASI_Sector       = 0;
-		SASI_Blocks       = 0;
-		SASI_CmdPtr       = 0;
-		SASI_Device       = 0;
-		SASI_Unit         = 0;
-		SASI_BufPtr       = 0;
-		SASI_RW           = 0;
-		SASI_Stat         = 0;
-		SASI_Error        = 0;
-		SASI_SenseStatPtr = 0;
-	}
-	else if (adr == 0xe96001)
-	{
-		if (SASI_Phase == 2)
+		SASI_Cmd[SASI_CmdPtr++] = data;
+		if (SASI_CmdPtr == 6) /* Command completed */
 		{
-			SASI_Cmd[SASI_CmdPtr++] = data;
-			if (SASI_CmdPtr == 6) /* Command completed */
-			{
 #if 0
 				//SASI_Phase++;
 #endif
-				SASI_CheckCmd();
-			}
-		}
-		else if ((SASI_Phase == 3) && (!SASI_RW)) /* Data writing in progress */
-		{
-			SASI_Buf[SASI_BufPtr++] = data;
-			if (SASI_BufPtr == 256)
-			{
-				result = SASI_Flush(); /* Write the current buffer */
-				SASI_Blocks--;
-				if (SASI_Blocks) /* More blocks to write? */
-				{
-					SASI_Sector++;
-					SASI_BufPtr = 0;
-					result      = SASI_Seek(); /* Read next sector into buffer */
-					if (!result)               /* result=0: if it is the end of the image (= invalid sector) */
-					{
-						SASI_Error = 0x0f;
-						SASI_Phase++;
-					}
-				}
-				else
-					SASI_Phase++; /* Specified block write complete */
-			}
-		}
-		else if (SASI_Phase == 10)
-		{
-			SASI_SenseStatPtr++;
-			if (SASI_SenseStatPtr == 10) /* Command issuance completed */
-			{
-				SASI_Phase = 4;
-			}
-		}
-		if (SASI_Phase == 4)
-		{
-			IOC_IntStat |= 0x10;
-			if (IOC_IntStat & 8)
-				IRQH_Int(1, &SASI_Int);
+			SASI_CheckCmd();
 		}
 	}
+	else if ((SASI_Phase == 3) && (!SASI_RW)) /* Data writing in progress */
+	{
+		SASI_Buf[SASI_BufPtr++] = data;
+		if (SASI_BufPtr == 256)
+		{
+			result = SASI_Flush(); /* Write the current buffer */
+			SASI_Blocks--;
+			if (SASI_Blocks) /* More blocks to write? */
+			{
+				SASI_Sector++;
+				SASI_BufPtr = 0;
+				result = SASI_Seek(); /* Read next sector into buffer */
+				if (!result) /* result=0: if it is the end of the image (=
+				                invalid sector) */
+				{
+					SASI_Error = 0x0f;
+					SASI_Phase++;
+				}
+			}
+			else
+				SASI_Phase++; /* Specified block write complete */
+		}
+	}
+	else if (SASI_Phase == 10)
+	{
+		SASI_SenseStatPtr++;
+		if (SASI_SenseStatPtr == 10) /* Command issuance completed */
+		{
+			SASI_Phase = 4;
+		}
+	}
+	if (SASI_Phase == 4)
+	{
+		IOC_IntStat |= 0x10;
+		if (IOC_IntStat & 8)
+			IRQH_Int(1, &SASI_Int);
+	}
+}
+
+uint8_t FASTCALL SASI_Read(uint32_t adr)
+{
+	if ((adr & 1) != 0)
+	{
+		adr &= 7;
+
+		if (adr == 1)
+		{
+			uint8_t ret = SASI_ReadData();
+			StatBar_HDD((SASI_Phase) ? 2 : 0);
+			return ret;
+		}
+		else if (adr == 3)
+		{
+			uint8_t ret = 0;
+
+			if (SASI_Phase)
+				ret |= 2; /* Busy */
+			if (SASI_Phase > 1)
+				ret |= 1; /* Req */
+			if (SASI_Phase == 2)
+				ret |= 8;                       /* C/D */
+			if ((SASI_Phase == 3) && (SASI_RW)) /* SASI_RW=1:Read */
+				ret |= 4;                       /* I/O */
+			if (SASI_Phase == 9)                /* Phase=9:SenseStatusExclusive */
+				ret |= 4;                       /* I/O */
+			if ((SASI_Phase == 4) || (SASI_Phase == 5))
+				ret |= 0x0c; /* I/O & C/D */
+			if (SASI_Phase == 5)
+				ret |= 0x10; /* MSG */
+			
+			StatBar_HDD((SASI_Phase) ? 2 : 0);
+			return ret;
+		}
+
+		/* other registers are write-only */
+		return 0xff;
+	}
+
+	/* Even addresses not decoded. */
+	return 0xff;
+}
+
+/* I/O Write */
+void FASTCALL SASI_Write(uint32_t adr, uint8_t data)
+{
+	int result, i;
+	uint8_t bit;
+
+	if ((adr & 1) != 0)
+	{
+		adr &= 7;
+
+		if (adr == 1)
+		{
+			SASI_WriteData(data);
+		}
+		else if (adr == 3)
+		{
+			if (SASI_Phase == 1)
+			{
+				SASI_Phase++;
+			}
+		}
+		else if (adr == 5)
+		{
+			SASI_Phase        = 0;
+			SASI_Sector       = 0;
+			SASI_Blocks       = 0;
+			SASI_CmdPtr       = 0;
+			SASI_Device       = 0;
+			SASI_Unit         = 0;
+			SASI_BufPtr       = 0;
+			SASI_RW           = 0;
+			SASI_Stat         = 0;
+			SASI_Error        = 0;
+			SASI_SenseStatPtr = 0;
+		}
+		else if (adr == 7)
+		{
+			if (SASI_Phase == 0)
+			{
+				SASI_WriteData(data);
+			}
+		}
+	}
+
 	StatBar_HDD((SASI_Phase) ? 2 : 0);
 }
 
