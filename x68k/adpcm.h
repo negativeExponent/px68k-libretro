842d04e690c6552ebaec3fc57d36a5f657c92714
negativeExponent <negativeExponent@users.noreply.github.com>
2024-08-15 13:15:36 +0800

    adpcm: Separate panning and clock ratio

--- x68k/adpcm.h
+++ x68k/adpcm.h
@@ -4,8 +4,9 @@
 void FASTCALL ADPCM_PreUpdate(int32_t clock);
 void FASTCALL ADPCM_Update(int16_t *buffer, int32_t length, int16_t *pbsp, int16_t *pbep);
 void ADPCM_SetVolume(uint8_t vol);
-void ADPCM_SetPan(int n);
-void ADPCM_SetClock(int n);
+void ADPCM_SetPan(uint8_t pan);
+void ADPCM_SetClock(uint8_t clock);
+void ADPCM_SetRatio(uint8_t ratio);
 void ADPCM_Init(uint32_t samplerate);
 int  ADPCM_IsReady(void);
 
